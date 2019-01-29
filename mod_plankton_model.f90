724e2345a6d0abb240b1a5cd7f2f36e72fff4139
blackcata <blackcata109@gmail.com>
2019-01-29 23:50:33 +0900

    Add LPM(Lagrangian Plankton Model) module for the PALM

---
+++ mod_plankton_model.f90
@@ -0,0 +1,20 @@
+!------------------------------------------------------------------------------!
+!                                                                              !
+!   PROGRAM : mod_plankton_model.f90                                           !
+!                                                                              !
+!   PURPOSE : Module for Lagrangian plankton model                             !
+!                                                                              !
+!                                                             2019.01.30 K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+
+
+        MODULE plankton_model
+
+          IMPLICIT NONE
+
+          SAVE
+
+        CONTAINS
+        
+        END MODULE plankton_model
